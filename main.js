1d803729b27c9bd9566ea5aa7c3e84fb946ec360
Kborne28 <65371558+Kborne28@users.noreply.github.com>
2021-06-12 20:51:01 -0500

    finished notes for week 8, and updated nav bar

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
+var canvas = document.getElementById("myCanvas");
+
+//is our canvas going to be 2D or 3D?
+var context = canvas.getContext("2d");
+
+//Filling our brush with color:
+context.strokeSyle = "red";
+
+//what will the color of the canvas fill be?
+context.fillRect(10, 10, 100, 100);
+context.strokeStyle = "red";
+context.fillStyle = "rgba(0, 0, 255, 0.5)";
+context.strokeRect(10, 10, 100, 100);
+
+
+
+
+
+
+
+
 
     
